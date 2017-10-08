6c7f4323d52c6d7d6b549b78ea4c3fa68a9fb248
Valerii Iatsko <viatsko@viatsko.me>
2017-10-08 21:50:27 +0200

    + LeetCode 463 Island Perimeter (w/o additional function)

--- leetcode/463-island-perimeter/island-perimeter.js
+++ leetcode/463-island-perimeter/island-perimeter.js
@@ -1,36 +1,33 @@
-
 const islandPerimeter = grid => {
-    const countBorders = (i, j) => {
-        let borders = 0;
-
-        if (i === 0 || grid[i - 1][j] === 0) {
-            borders++;
-        }
-
-        if ((i === grid.length - 1) || grid[i + 1][j] === 0) {
-            borders++;
-        }
-
-        if (j === 0 || grid[i][j - 1] === 0) {
-            borders++;
-        }
-
-        if ((j === grid[0].length - 1) || grid[i][j + 1] === 0) {
-            borders++;
-        }
-
-        return borders;
-    }
-
-    let borders = 0;
+    let p = 0;
 
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] === 1) {
-                borders += countBorders(i, j);
+            if (grid[i][j] !== 1) {
+                continue;
             }
+
+            let isles = 0;
+
+            if (i > 0 && grid[i - 1][j] === 1) {
+                isles++;
+            }
+
+            if (i < (grid.length - 1) && grid[i + 1][j] === 1) {
+                isles++;
+            }
+
+            if (j > 0 && grid[i][j - 1] === 1) {
+                isles++;
+            }
+
+            if (j < (grid[0].length - 1) && grid[i][j + 1] === 1) {
+                isles++;
+            }
+
+            p += (4 - isles);
         }
     }
 
-    return borders;
-}
+    return p;
+};
