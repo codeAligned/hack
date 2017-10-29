d5fad069a0dc94c8d018ad610808179b4e21bc25
Valerii Iatsko <viatsko@viatsko.me>
2017-10-29 20:41:00 +0100

    + HackerRank / Java / Data Structures / Java Bit Set (renamed)

--- hackerrank/java/data-structures/JavaBitSet.java
+++ hackerrank/java/data-structures/JavaBitSet.java
@@ -1,7 +1,7 @@
 import java.util.BitSet;
 import java.util.Scanner;
 
-public class JavaBitset {
+public class JavaBitSet {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
