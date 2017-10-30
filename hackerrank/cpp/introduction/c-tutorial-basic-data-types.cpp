22bdd8088865ee279fa3ca5abfa8a01fe5e7c751
Valerii Iatsko <viatsko@viatsko.me>
2017-10-30 20:57:52 +0100

    + HackerRank / C++ / Introduction / C++ Basic Data Types

--- hackerrank/cpp/introduction/c-tutorial-basic-data-types.cpp
+++ hackerrank/cpp/introduction/c-tutorial-basic-data-types.cpp
@@ -7,17 +7,15 @@
 using namespace std;
 
 int main() {
-    // Complete the code.
     int a;
     long b;
-    long long c;
-    char d;
-    float e;
-    double f;
+    char c;
+    float d;
+    double e;
 
-    scanf("%d %ld %lld %c %f %lf", &a, &b, &c, &d, &e, &f);
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
 
-    printf("%d\n%ld\n%lld\n%c\n%f\n%lf", a, b, c, d, e, f);
+    printf("%d\n%ld\n%c\n%.3f\n%.9lf", a, b, c, d, e);
 
     return 0;
 }
