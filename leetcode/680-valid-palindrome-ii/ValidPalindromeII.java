90bbe9585c7700feda8e59488fb8fe9b4e3861fd
Valerii Iatsko <viatsko@viatsko.me>
2017-11-01 22:40:50 +0100

    + LeetCode 680 Valid Palindrome II (simplify)

--- leetcode/680-valid-palindrome-ii/ValidPalindromeII.java
+++ leetcode/680-valid-palindrome-ii/ValidPalindromeII.java
@@ -2,11 +2,8 @@ public class ValidPalindromeII {
     private boolean isPalindrome(String s, int i, int j, boolean first) {
         while (++i < --j) {
             if (s.charAt(i) != s.charAt(j)) {
-                if (first) {
-                    return isPalindrome(s, i, j + 1, false) || isPalindrome(s, i - 1, j, false);
-                }
+                return first && (isPalindrome(s, i, j + 1, false) || isPalindrome(s, i - 1, j, false));
 
-                return false;
             }
         }
 
