21fc8ded074943212de80172e1d19a8237a4859d
Valerii Iatsko <viatsko@viatsko.me>
2017-11-11 23:38:12 +0100

    + HackerRank / Tutorials / Cracking the Coding Interview / Data Structures / Heaps: Find the Running Median (suppress duplicates warning)

--- hackerrank/tutorials/cracking-the-coding-interview/data-structures/HeapsFindTheRunningMedian.java
+++ hackerrank/tutorials/cracking-the-coding-interview/data-structures/HeapsFindTheRunningMedian.java
@@ -3,6 +3,7 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class HeapsFindTheRunningMedian {
+    @SuppressWarnings("Duplicates")
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
