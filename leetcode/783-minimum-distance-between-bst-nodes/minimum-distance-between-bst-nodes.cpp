e5e4c5c18213e16990279843f4f7e28763203f93
Valerii Iatsko <viatsko@viatsko.me>
2018-04-08 17:43:00 +0200

    Updated leetcode 783 solution

--- leetcode/783-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
+++ leetcode/783-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
@@ -12,6 +12,13 @@
  * };
  */
 
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
 class Solution {
   int min = INT_MAX;
   int prev;
