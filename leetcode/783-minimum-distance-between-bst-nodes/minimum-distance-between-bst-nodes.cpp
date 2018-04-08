c2da31c07e6e87b18f5761d186f75e019ceb73d3
Valerii Iatsko <viatsko@viatsko.me>
2018-04-08 17:06:04 +0200

    + LeetCode 783. Minimum Distance Between BST Nodes

---
+++ leetcode/783-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+  int min = INT_MAX;
+  int prev;
+  bool prevIsSet = false;
+public:
+  void traverse(TreeNode *node) {
+    if (node == nullptr) {
+      return;
+    }
+
+    traverse(node->left);
+
+    if (prevIsSet) {
+      min = std::min(min, node->val - prev);
+    }
+
+    prev = node->val;
+    prevIsSet = true;
+
+    traverse(node->right);
+  }
+
+  int minDiffInBST(TreeNode* root) {
+    if (root == nullptr) {
+      return 0;
+    }
+
+    traverse(root);
+
+    return min;
+  }
+};
