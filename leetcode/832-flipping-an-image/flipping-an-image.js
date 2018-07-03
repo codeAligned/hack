2f828b17281f366846cd9f50c48bee808b62cb6b
Valerii Iatsko <viatsko@viatsko.me>
2018-07-03 07:11:21 +0200

    + LeetCode 832. Flipping an Image 2/2

--- leetcode/832-flipping-an-image/flipping-an-image.js
+++ leetcode/832-flipping-an-image/flipping-an-image.js
@@ -4,16 +4,11 @@
  */
 const flipAndInvertImage = function(A) {
   for (let i = 0; i < A.length; i++) {
-    const rowLen = A[0].length;
-
-    for (let j = 0; j < rowLen >> 1; j++) {
-      const tmp = +!A[i][j];
-      A[i][j] = +!A[i][rowLen - j - 1];
-      A[i][rowLen - j - 1] = tmp;
-    }
-
-    if (rowLen & 1) {
-      A[i][rowLen >> 1] = +!A[i][rowLen >> 1];
+    const rowLen = A[0].length - 1;
+    for (let j = 0; j <= rowLen >> 1; j++) {
+      const tmp = A[i][j] ^ 1;
+      A[i][j] = A[i][rowLen - j] ^ 1;
+      A[i][rowLen - j] = tmp;
     }
   }
 
