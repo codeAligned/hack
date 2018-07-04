248eeb032d78dabf86ac1eacac47e009592e9ce1
Valerii Iatsko <viatsko@viatsko.me>
2018-07-04 19:58:56 +0200

    + LeetCode 278. First Bad Version

--- leetcode/278-first-bad-version/first-bad-version.js
+++ leetcode/278-first-bad-version/first-bad-version.js
@@ -1,23 +1,36 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
 
-const solution = isBadVersion => {
-    /**
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
-    return n => {
-        let start = 1;
-        let end = n;
-        
-        while (start < end) {
-            const mid = start + Math.floor((end - start) / 2);
-            
-            if (!isBadVersion(mid)) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-        
-        return start;
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+const solution = function(isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+    let min = 0;
+    let max = n;
+
+    while (min < max) {
+      const mid = (min + max) >>> 1;
+
+      if (!isBadVersion(mid)) {
+        min = mid + 1;
+      } else {
+        max = mid;
+      }
     }
-}
+
+    return min;
+  };
+};
